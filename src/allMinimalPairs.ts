6e2b0855fab7fcf372f8d93b03c76a2659d895a6
Pokey Rule <755842+pokey@users.noreply.github.com>
2024-02-19 12:15:11 +0000

    Very usable version

--- src/allMinimalPairs.ts
+++ src/allMinimalPairs.ts
@@ -1,10 +1,12 @@
-interface Word {
+export interface Word {
   french: string;
   pronunciation: string;
 }
 
-type MinimalPair = [Word, Word];
+export type MinimalPair = [Word, Word];
 
+// From http://minimalpairs.net/en/fr
+// (via https://web.archive.org/web/20200804073656/minimalpairs.net/en/fr)
 export const minimalPairs: Record<string, MinimalPair[]> = {
   "e-ɛ": [
     [
@@ -46,6 +48,59 @@ export const minimalPairs: Record<string, MinimalPair[]> = {
     [
       { french: "était", pronunciation: "/etɛ/" },
       { french: "été", pronunciation: "/ete/" },
+    ],
+  ],
+
+  "ø-œ": [
+    [
+      { french: "jeûne", pronunciation: "/ʒøn/" },
+      { french: "jeune", pronunciation: "/ʒœn/" },
+    ],
+    [
+      { french: "veulent", pronunciation: "/vœl/" },
+      { french: "veule", pronunciation: "/vøl/" },
+    ],
+  ],
+
+  "ø-ɔ": [
+    [
+      { french: "neutre", pronunciation: "/nøtʁ/" },
+      { french: "notre", pronunciation: "/nɔtʁ/" },
+    ],
+  ],
+
+  "œ-ɔ": [
+    [
+      { french: "beurre", pronunciation: "/bœʁ/" },
+      { french: "bord", pronunciation: "/bɔʁ/" },
+    ],
+    [
+      { french: "heure", pronunciation: "/œʁ/" },
+      { french: "or", pronunciation: "/ɔʁ/" },
+    ],
+    [
+      { french: "lors", pronunciation: "/lɔʁ/" },
+      { french: "leur", pronunciation: "/lœʁ/" },
+    ],
+    [
+      { french: "mœurs", pronunciation: "/mœʁ/" },
+      { french: "mort", pronunciation: "/mɔʁ/" },
+    ],
+    [
+      { french: "port", pronunciation: "/pɔʁ/" },
+      { french: "peur", pronunciation: "/pœʁ/" },
+    ],
+    [
+      { french: "seul", pronunciation: "/sœl/" },
+      { french: "sol", pronunciation: "/sɔl/" },
+    ],
+    [
+      { french: "sort", pronunciation: "/sɔʁ/" },
+      { french: "sœur", pronunciation: "/sœʁ/" },
+    ],
+    [
+      { french: "veulent", pronunciation: "/vœl/" },
+      { french: "vol", pronunciation: "/vɔl/" },
     ],
   ],
 };
--- src/getWordRecordings.ts
+++ src/getWordRecordings.ts
@@ -4,33 +4,164 @@ import { unlink } from "fs/promises";
 import playSound from "play-sound";
 import { Readable } from "stream";
 import { finished } from "stream/promises";
-import { minimalPairs } from "./allMinimalPairs";
+import { MinimalPair, Word, minimalPairs } from "./allMinimalPairs";
 import { getWordRecordingUrls } from "./getWordRecordingUrls"; // Adjust the import path as necessary
 import { choice } from "./random";
 
 import type { ReadableStream } from "node:stream/web";
+import prompt from "inquirer-interactive-list-prompt";
+import chalk from "chalk";
+import binomialTest from "@stdlib/stats-binomial-test";
 
-const pairs = await select({
-  message: "Pick a pair",
-  choices: Object.entries(minimalPairs).map(([name, pairs]) => ({
-    name,
-    value: pairs,
-  })),
-});
+const player = playSound();
+const keys = ["j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v"];
 
 while (true) {
-  // Pick a random pair
-  const pair = choice(pairs);
+  const pairs: MinimalPair[] | "quit" = await prompt({
+    message: "Pick a pair",
+    choices: [
+      ...Object.entries(minimalPairs).map(([name, pairs], i) => ({
+        name,
+        value: pairs,
+        key: keys[i],
+      })),
+      {
+        name: "Quit",
+        value: "quit",
+        key: "q",
+      },
+    ],
+  });
 
-  // Randomly pick one of the words in the pair
-  const wordInfo = choice(pair);
-  const word = wordInfo.french;
+  if (pairs === "quit") {
+    process.exit(0);
+  }
 
+  class NoRecordingsError extends Error {}
+
+  let successes = 0;
+  let total = 0;
+
+  while (true) {
+    // Pick a random pair
+    const pair = choice(pairs);
+
+    const wordsWithRecordings = pair.map((word, index) => ({
+      word,
+      path: `/tmp/minimal-pair-recording-${index}.wav`,
+    }));
+
+    try {
+      await Promise.all(
+        wordsWithRecordings.map(({ word, path }) =>
+          downloadRandomWordRecording(word.french, path)
+        )
+      );
+    } catch (e) {
+      if (e instanceof NoRecordingsError) {
+        continue;
+      }
+      throw e;
+    }
+
+    // Randomly pick one of the words in the pair
+    const correctWord = choice(wordsWithRecordings);
+
+    player.play(correctWord.path);
+
+    const chosenWord: WordWithRecording | "done" = await prompt({
+      message: "Which was it?",
+      choices: [
+        ...wordsWithRecordings.map((option, i) => ({
+          name: wordToString(option.word),
+          value: option,
+          key: keys[i],
+        })),
+        {
+          name: "Replay",
+          value: "replay",
+          key: "l",
+          fn() {
+            player.play(correctWord.path);
+          },
+        },
+        {
+          name: "Done",
+          value: "done",
+          key: "d",
+        },
+      ],
+    });
+
+    if (chosenWord === "done") {
+      break;
+    }
+
+    total += 1;
+
+    const success = chosenWord === correctWord;
+
+    if (success) {
+      successes += 1;
+      console.log(`\n${chalk.green.bold("Correct!")}\n`);
+    } else {
+      console.log(
+        `\n${chalk.red.bold("Wrong!")} It was ${chalk.cyan.bold(
+          wordToString(correctWord.word)
+        )}\n`
+      );
+      player.play(chosenWord.path);
+    }
+
+    const { pValue } = binomialTest(successes, total);
+
+    console.log(
+      `Success rate: ${successes}/${total} (${(
+        (successes / total) *
+        100
+      ).toFixed(2)}%, p-value: ${pValue.toFixed(3)})\n`
+    );
+
+    if (!success) {
+      await prompt({
+        message: "Replay?",
+        default: chosenWord,
+        choices: [
+          ...wordsWithRecordings.map((option, i) => ({
+            name: wordToString(option.word),
+            key: keys[i],
+            fn() {
+              player.play(option.path);
+            },
+            value: option,
+          })),
+          {
+            name: "Continue",
+            value: "done",
+            key: "l",
+          },
+        ],
+      });
+
+      console.log("\n");
+    }
+  }
+}
+
+interface WordWithRecording {
+  word: Word;
+  path: string;
+}
+
+function wordToString(option: Word): string {
+  return `${option.french} (${option.pronunciation})`;
+}
+
+async function downloadRandomWordRecording(word: string, path: string) {
   const recordings = await getWordRecordingUrls(word);
 
   if (recordings.length === 0) {
-    console.log("No recordings found!");
-    continue;
+    throw new NoRecordingsError("No recordings found");
   }
 
   // Pick a random recording
@@ -39,7 +170,6 @@ while (true) {
   // Download to temp file using fetch
   const response = await fetch(recording);
 
-  const path = "/tmp/minimal-pair-recording.wav";
   // Remove the file if it already exists
   try {
     await unlink(path);
@@ -54,24 +184,4 @@ while (true) {
       fileStream
     )
   );
-
-  const player = playSound();
-  player.play(path);
-  const answer = await select({
-    message: "Which was it?",
-    choices: pair.map((option) => ({
-      name: wordToString(option),
-      value: option,
-    })),
-  });
-
-  if (answer.french === wordInfo.french) {
-    console.log("Correct!");
-  } else {
-    console.log(`Incorrect! It was ${wordToString(wordInfo)}\n`);
-  }
-}
-
-function wordToString(option: Word): string {
-  return `${option.french} (${option.pronunciation})`;
 }
